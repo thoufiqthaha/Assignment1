0acb434c1c662ed795665b753f5fc11ea14fb5ca
Afsal <Afsal@mail.com>
2021-08-30 23:53:15 +0530

    Assignement 1

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+
+pipeline {
+    agent any
+
+    parameters{
+        string(name: 'environment',description: 'Select your Environment')
+        string(name: 'appname',description: 'Select your Application Name')
+    }
+
+
+    stages {
+        stage("echo"){
+            steps {
+                script {
+                    echo "Going to run a shell script:"
+                    sh """
+                    chmod +x Assin1.sh
+                    ./Assin1.sh "${params.environment}" "${params.appname}" """
+                }
+
+            }
+        }
+    }
+}
